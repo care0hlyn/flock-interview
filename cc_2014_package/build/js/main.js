5539841a08c1ededc682e1f1f0207d3f31c80848
Carolyn Huynh <ckhuynh20@gmail.com>
2014-11-21 11:56:46 -0800

    Install galleria plugin

--- cc_2014_package/build/js/main.js
+++ cc_2014_package/build/js/main.js
@@ -1,14 +1,16 @@
 function main_ready(data){
-  console.log('main ready!')
 
-  $(".container").show();
-  $("#landing").show();
-  $(".PORTLAND___LOS_ANGELES").show();
-  $(".Rectangle_4_copy").show();
-  $("#THE_FLOCK").show();
-  $("#Lorem_ipsum").show();
-  $(".top_details").show();
-  $("#full_copy_4").show();
+  $(".hidden").show();
 
+  $(".Rectangle_3").click(function() {
+
+    $(".galleria").show();
+    $(".hidden").hide();
+    
+    $(".container").die();
+
+  });
+  
 }
+
 
